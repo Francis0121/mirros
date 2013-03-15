1028e7c70a0ad8617d3217f8088648d76f2cbd52
Francis <kims_0121@naver.com>
2013-03-15 10:30:11 +0900

    scroll시 loadingImg 및 끝났을때 표시

--- jtown/src/main/webapp/resources/js/jquery.scroll.pagination.js
+++ jtown/src/main/webapp/resources/js/jquery.scroll.pagination.js
@@ -33,10 +33,9 @@
 		var target = opts.scrollTarget;
 		var mayLoadContent = $(target).scrollTop() + opts.heightOffset >= $(document).height() - $(target).height();
 		if (mayLoadContent) {
-			if (opts.beforeLoad != null) {
-				opts.beforeLoad();
-			}
 			$(obj).children().attr('rel', 'loaded');
+			opts.loading.start();
+			
 			var url = opts.contentPage;
 			var json = opts.contentData;
 			var currentPage = $('#__current_page');
@@ -52,10 +51,18 @@
 						if (opts.afterLoad != null) {
 							opts.afterLoad(objectsRendered);
 						}
+						opts.loading.finished();
 						$container.masonry('reload');
 					}else{
 						opts.errorCallback;
 					}
+				});
+			}else{				
+				opts.loading.msg.find('img').hide().parent().find('div').html(
+						opts.loading.finishedMsg).animate({
+					opacity : 1
+				}, 2000, function() {
+					$(this).parent().fadeOut('normal');
 				});
 			}
 		}
@@ -64,7 +71,7 @@
 	$.fn.scrollPagination.init = function(obj, opts) {
 		var target = opts.scrollTarget;
 		$(obj).attr('scrollPagination', 'enabled');
-
+		$(obj).append('<input type="hidden" id="__current_page" value="3"/>');
 		$(target).scroll(function(event) {
 			if ($(obj).attr('scrollPagination') == 'enabled') {
 				$.fn.scrollPagination.loadContent(obj, opts);
@@ -72,13 +79,29 @@
 				event.stopPropagation();
 			}
 		});
+		
+		opts.loading.selector = opts.loading.selector || $(obj);
+		opts.loading.msg = $('<div id="infscr-loading"><img alt="Loading..." src="'
+				+ opts.loading.img
+				+ '" /><div>'
+				+ opts.loading.msgText
+				+ '</div></div>');
+		
+		(new Image()).src = opts.loading.img;
+		opts.loading.start = opts.loading.start
+				|| function() {
+					opts.loading.msg.appendTo(opts.loading.selector).show(opts.loading.speed);
+				};
+		opts.loading.finished = opts.loading.finished || function() {
+			opts.loading.msg.fadeOut('normal');
+		};
+		
 		$.fn.scrollPagination.loadContent(obj, opts);
 	};
 
 	$.fn.scrollPagination.defaults = {
 		'contentPage' : null,
 		'contentData' : {},
-		'beforeLoad' : null,
 		'afterLoad' : null,
 		'scrollTarget' : null,
 		'heightOffset' : 0,
@@ -89,6 +112,16 @@
 		'errorCallback' : function(){
 			alert('ERROR');
 		},
-		'maxPage': 1
+		'maxPage': 1,
+		loading : {
+			finished : undefined,
+			finishedMsg: 'No more pages to load.',
+			img: contextPath + 'resources/images/jt-loading-big.gif',
+			msg : null,
+			msgText : '<em>Loading the next set of posts...</em>',
+			selector : null,
+			speed : 'slow',
+			start : undefined
+		}
 	};
 })(jQuery);
