53adf47d95bb6aedc23485d8df921d19e428c837
Francis <kims_0121@naver.com>
2013-04-01 16:48:20 +0900

    페이지상단으로 이동하는 텝 생성

--- jtown/src/main/java/com/bg/jtown/util/DateUtil.java
+++ jtown/src/main/java/com/bg/jtown/util/DateUtil.java
@@ -3,7 +3,6 @@ package com.bg.jtown.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.TimeZone;
