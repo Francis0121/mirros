164f51c146968cf772bd829284ce77a5f604dd2f
Francis <kims_0121@naver.com>
2013-04-10 16:13:56 +0900

    mysql 을 통해 쿼리 조회후 실시간 좋아요 순위 업데이트

--- realTime/node/program.js
+++ realTime/node/program.js
@@ -4,55 +4,97 @@
  * 	@author Francisapache ssl node
  * 
  **************************/
-//server setting
-const modulePath = ''; // 로컬환경
+const modulePath = '';
+const redisHost = '127.0.0.1';
+const proxyDomain = 'localhost';
 const serverPort = 8000; 
 const proxyPort = 9000;
-const redisHost = '127.0.0.1';
 const redisPort = 6379;
 
 var fs = require('fs'),
 	http = require('http'),
 	https = require('https'),
-	httpProxy = require(modulePath+'http-proxy');
+	httpProxy = require(modulePath+'http-proxy'),	
+	redis = require(modulePath+'redis'),
+	socketio = require(modulePath+'socket.io'),
+	mysql = require(modulePath+'mysql');
 
-var options = {
+var ssl_options = {
 	https: {
 		key: fs.readFileSync('C:/nodeJs/real/mirros.key').toString(),
 		cert: fs.readFileSync('C:/nodeJs/real/mirros.crt').toString(),
 		passphrase : 'q7bjvdqbe83lt0aj'
 	}
 };
-
-httpProxy.createServer(serverPort, 'localhost', options).listen(proxyPort);
-
-var server = http.createServer(function(request, response){
-	console.log('Create Server Doing ... ');
-}).listen(serverPort);
-console.log('Server Listening on port ' + serverPort);
-
-var socketio = require(modulePath+'socket.io');
-var io = socketio.listen(server);
-
-var redis = require(modulePath+'redis');
-var subscriber = redis.createClient(redisPort, redisHost);
-subscriber.auth('');
-subscriber.on('error', function(err) {
+var mysql_options = {
+	host : 'localhost',
+	user : 'kims0121',
+	password : 'sg3512af@'
+};
+/*************************
+ * 	6379 Redis Client 
+ *************************/
+var socketRedis = redis.createClient(redisPort, redisHost);
+socketRedis.auth('');
+socketRedis.on('error', function(err) {
 	console.log('Error In Redis Server');
 });
-subscriber.subscribe('real_time');
-
-io.set('log level', 1);
+socketRedis.subscribe('real_time');
+socketRedis.subscribe('love_rank');
+socketRedis.on('message', function(channel, message) {
+	console.log('Channel : '+ channel + ' Message : ' + message);
+	io.sockets.emit(channel, message);
+});
+/************************
+ * 	mysql
+ ************************/
+setInterval(updateLoveRealCount, 600000);
+var connection = mysql.createConnection(mysql_options);
+var mysqlRedis = redis.createClient(redisPort, redisHost);
+mysqlRedis.auth('');
+mysqlRedis.on('error', function(err) {
+	console.log('Error In Redis Server');
+});
+function updateLoveRealCount(){
+	connection.query('SELECT seller_pn FROM jtown.count_love_interval_1hour WHERE love_count >= 1', function(error, result, fields){
+		if(error){
+			console.log('Error In Mysql');
+		}else{
+			var sellerPnList = '';
+			for(var i=0, len = result.length; i<len;i++){
+				var obj = result[i];
+				var sellerPn = obj.seller_pn;
+				if((i+1) == len){
+					sellerPnList += sellerPn;
+				}else{
+					sellerPnList += sellerPn+',';
+				};
+			};
+			console.log(sellerPnList);
+			mysqlRedis.publish('love_rank', sellerPnList);
+		};
+	});
+};
+/*************************
+ * 	9000 Https Proxy Server 
+ *************************/
+httpProxy.createServer(serverPort, proxyDomain, ssl_options).listen(proxyPort);
+/*************************
+ * 	8000 Http Server 
+ *************************/
+var server = http.createServer().listen(serverPort);
+/*************************
+ * 	8000 Socket.io 
+ *************************/
+var io = socketio.listen(server);
+io.set('log level', 2);
 io.sockets.on('connection', function(socket){
-//	console.log('Connection');
+	socket.log.info('Connection Socket');
 	
 	socket.on('disconnect', function() {
-//		console.log('disconnect');
+		socket.log.info('Disconncet Socket');
+		socket.leave();
 	});
-});
-
-subscriber.on('message', function(channel, message) {
-	io.sockets.emit('real_time', message);
 });
 
 //Server
