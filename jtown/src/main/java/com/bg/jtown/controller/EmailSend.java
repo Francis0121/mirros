f383a2eb65924bd99b50d3a2dc8bf87d06b4e0e0
Francis <kims_0121@naver.com>
2013-05-16 14:53:50 +0900

    인증 메일 양식 변경 ( 내 마음대로 진섭이가 안주니까)

--- jtown/src/main/java/com/bg/jtown/controller/EmailSend.java
+++ jtown/src/main/java/com/bg/jtown/controller/EmailSend.java
@@ -43,16 +43,32 @@ public class EmailSend {
 			encryptText = null;
 		}
 
-		String subject = "미러스 인증 메일 입니다.";
+		String subject = "[Mirros] 가입인증메일입니다.";
 		StringBuffer sb = new StringBuffer();
-		sb.append("안녕하세요 미러스 입니다<br/>");
-		sb.append("<a href='");
-		sb.append("http://www.mirros.net/confirmEmailAddress?");
-		sb.append("id=").append(username).append("&");
-		sb.append("series=").append(encryptText);
-		sb.append("' target='_self'>클릭하면 인증됩니다.");
-		sb.append("</a>");
 
+		sb.append("	<div style='font-family: Dotum, Gulim; font-size: 12px; line-height: 2; width: 600px;'>");
+		sb.append("		<div style='border-bottom: 2px solid #ffa500; padding: 10px;'>");
+		sb.append("			<a href='https://www.mirros.net'><img alt='Mirros' src='https://www.mirros.net/resources/images/jt-main-banner.png'></a>");
+		sb.append("		</div>");
+		sb.append("		<div style='padding: 10px;'>");
+		sb.append("			<h1 style='font-weight: normal;'>Weclode to Mirros</h1>");
+		sb.append("			<p>");
+		sb.append("				Mirros 계정 ("+jtownUser.getName()+")에 대한 이메일 인증 이메일 입니다.<br/>");
+		sb.append("				아래에 링크를 클릭하시면 이메일 인증이 완료 됩니다.");
+		sb.append("			</p>");
+		sb.append("			<p>");
+		sb.append("				<a href='http://www.mirros.net/confirmEmailAddress?id="+username+"&series="+encryptText+"' target='_self' style='color: #ef662a;'>링크를 클릭해주세요</a>");
+		sb.append("			</p>");
+		sb.append("			<p>");
+		sb.append("				지속적으로 요청하지 않은 인증 메일을 자주 받으신다면, <a href='mailto:help@mirros.net' style='color: #ef662a;'>help@mirros.net</a>로 문의 주시기 바랍니다.");
+		sb.append("			</p>");
+		sb.append("		</div>");
+		sb.append("		<div style='color: #808080; font-size: 11px; border-top: 1px solid #d9d9d9; padding: 10px;'>");
+		sb.append("			본 메일은 발신전용 이메일입니다.");
+		sb.append("			궁금한 사항을 문의하시거나 Mirros계정 관련 도움은 <a href='mailto:help@mirros.net' style='color: #ef662a;'>help@mirros.net</a>로 문의 주시거나, <a href='https://www.mirros.net/help/question' style='color: #ef662a;'>FAQ</a>페이지에서 확인하시기 바랍니다.");
+		sb.append("		</div>");
+		sb.append("	</div>");
+		
 		emailSender.sendEmail(username, subject, sb.toString());
 	}
 
@@ -61,10 +77,35 @@ public class EmailSend {
 		String tempPassword = customJdbcUserDetailManager
 				.changeTempPassword(username);
 
-		String subject = "미러스 :: 임시 비밀번호 입니다.";
+		JtownUser jtownUser = (JtownUser) customJdbcUserDetailManager
+				.loadUserByUsername(username);
+
+		String subject = "[Mirros] 임시비밀번호를 알려 드립니다.";
 		StringBuffer sb = new StringBuffer();
-		sb.append("안녕하세요 미러스 입니다<br/>");
-		sb.append(tempPassword);
+		sb.append("	<div style='font-family: Dotum, Gulim; font-size: 12px; line-height: 2; width: 600px;'>");
+		sb.append("		<div style='border-bottom: 2px solid #ffa500; padding: 10px;'>");
+		sb.append("			<a href='https://www.mirros.net'><img alt='Mirros' src='https://www.mirros.net/resources/images/jt-main-banner.png'></a>");
+		sb.append("		</div>");
+		sb.append("		<div style='padding: 10px;'>");
+		sb.append("			<h1 style='font-weight: normal;'>"+jtownUser.getName()+"님, 비밀번호를 잊으셨나요?</h1>");
+		sb.append("			<p>");
+		sb.append("				Mirros 계정 ("+jtownUser.getUsername()+")에 대한 비밀번호 재설정 요청을 받았습니다.<br/>");
+		sb.append("				비밀번호가 재설정 되었으며, 재 설정된 비밀번호는 아래와 같습니다.");
+		sb.append("			</p>");
+		sb.append("			<p>");
+		sb.append("				비밀번호 : "+tempPassword);
+		sb.append("			</p>");
+		sb.append("			<p>");
+		sb.append("				지속적으로 요청하지 않은 비밀번호 재설정 메일을 자주 받으신다면, ");
+		sb.append("				<a href='https://www.mirros.net/login/modify' style='color: #ef662a;'>계정설정</a>에서 비밀번호를 재설정 해주시기 바랍니다.");
+		sb.append("				그리고 본 메일은 보안을 위해 확인후 삭제 하시기 바라며, 임시 비밀번호로 로그인 후에는 새로운 비밀번호로 변경해 주시기 바랍니다.");
+		sb.append("			</p>");
+		sb.append("		</div>");
+		sb.append("		<div style='color: #808080; font-size: 11px; border-top: 1px solid #d9d9d9; padding: 10px;'>");
+		sb.append("			본 메일은 발신전용 이메일입니다.");
+		sb.append("			궁금한 사항을 문의하시거나 Mirros계정 관련 도움은 <a href='mailto:help@mirros.net' style='color: #ef662a;'>help@mirros.net</a>로 문의 주시거나, <a href='https://www.mirros.net/help/question' style='color: #ef662a;'>FAQ</a>페이지에서 확인하시기 바랍니다.");
+		sb.append("		</div>");
+		sb.append("</div>");
 
 		emailSender.sendEmail(username, subject, sb.toString());
 	}
